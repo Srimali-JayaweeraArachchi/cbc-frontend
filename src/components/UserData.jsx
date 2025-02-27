0def2e4992da7cd90620df58762961413ff3c9b1
Srimali <jasrimalimadurangani@gmail.com>
2025-02-27 19:47:03 +0530

    Create some html tag function 🆕✅

---
+++ src/components/UserData.jsx
@@ -0,0 +1,10 @@
+export default function UserData() {
+    return (
+        <div>
+       <img src = "https://th.bing.com/th/id/OIP.7WA6mH7oqsGTo_duc43GqwHaHa?rs=1&pid=ImgDetMain"/>
+       <span>John Doe</span>
+       <span>Online</span>
+       <span>Active</span>
+       </div>
+    )
+}
---
+++ src/components/productCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard() {
+    return (
+        <>
+            <h1>Product One</h1>
+            <h2>Price: $100</h2>
+            <button>Add to Cart</button>
+        </>
+    )
+}
